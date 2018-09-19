4bae14fafc48254fa59fe92618bd6fc48059b5d7
beumana <bryantumana@gmail.com>
2018-09-18 23:32:00 -0400

    add player scores and right paddle AI

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ var canvas;
 var canvasContext;
 var ballX = 50;
 var ballY = 50;
-var ballSpeedX = 10;
-var ballSpeedY = 4;
+var ballSpeedX = 14;
+var ballSpeedY = 6;
 
 var paddle1Y = 250;
+var paddle2Y = 250;
+const PADDLE_THICKNESS = 10;
 const PADDLE_HEIGHT = 100;
+
+var player1Score = 0;
+var player2Score = 0;
 
 function calculateMousePos(evt) {
     var rect = canvas.getBoundingClientRect();
@@ -37,16 +42,41 @@ window.onload = function() {
         })
 }
 
+function ballReset() {
+    ballSpeedX = -ballSpeedX;
+    ballX = canvas.width/2;
+    ballY = canvas.height/2
+}
+
+function computerMovement() {
+    var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT/2);
+    if(paddle2YCenter < ballY - 35) {
+        paddle2Y += 10;
+    } else if (paddle2YCenter > ballY + 35){
+        paddle2Y -= 10;
+    }
+}
+
 function moveEverything(){
+    computerMovement();
+
     ballX += ballSpeedX;
     ballY += ballSpeedY;
-    ballSpeedX += .03;
-    ballSpeedY += .03;
 
     if (ballX >= canvas.width) {
-        ballSpeedX = -ballSpeedX;
+        if (ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT){
+            ballSpeedX = -ballSpeedX;
+        } else {
+            ballReset();
+            player1Score += 1;
+        }
     } else if (ballX <= 0) {
-        ballSpeedX = -ballSpeedX;
+        if (ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT){
+            ballSpeedX = -ballSpeedX;
+        } else {
+            ballReset();
+            player2Score += 1;
+        }
     }
 
     if (ballY >= canvas.height) {
@@ -59,9 +89,14 @@ function moveEverything(){
 function drawEverything() {
     colorRect(0, 0, canvas.width,canvas.height, 'black'); // draw background
     
-    colorRect(20, paddle1Y, 10, PADDLE_HEIGHT, 'white'); // draw paddle
+    colorRect(20, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white'); // left player paddle
+
+    colorRect(canvas.width - 30, paddle2Y, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white'); // right computer paddle
     
     colorCircle(ballX, ballY, 10, 'white'); // draw ball
+
+    canvasContext.fillText(player1Score, 100, 100);
+    canvasContext.fillText(player2Score, canvas.width-100, 100)
 }
 
 // Creates circles
