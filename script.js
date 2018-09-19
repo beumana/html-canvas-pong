6a40d04c0d78918544d7233b44700386205e8a4f
beumana <bryantumana@gmail.com>
2018-09-18 22:30:43 -0400

    add y axis to ball and calculate mouse position for paddle

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballY = 50;
 var ballSpeedX = 10;
+var ballSpeedY = 4;
+
+var paddle1Y = 250;
+const PADDLE_HEIGHT = 100;
+
+function calculateMousePos(evt) {
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+
+    return {
+        x: mouseX,
+        y: mouseY
+    };
+}
 
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
@@ -12,22 +29,39 @@ window.onload = function() {
         moveEverything();
         drawEverything();
     }, 1000/framesPerSecond);
+
+    canvas.addEventListener('mousemove', 
+        function(evt){
+            var mousePos = calculateMousePos(evt);
+            paddle1Y = mousePos.y - (PADDLE_HEIGHT / 2);
+        })
 }
 
 function moveEverything(){
     ballX += ballSpeedX;
-    ballSpeedX += .01
+    ballY += ballSpeedY;
+    ballSpeedX += .03;
+    ballSpeedY += .03;
+
     if (ballX >= canvas.width) {
         ballSpeedX = -ballSpeedX;
     } else if (ballX <= 0) {
         ballSpeedX = -ballSpeedX;
     }
+
+    if (ballY >= canvas.height) {
+        ballSpeedY = -ballSpeedY;
+    } else if (ballY <= 0) {
+        ballSpeedY = -ballSpeedY;
+    }
 }
 
 function drawEverything() {
     colorRect(0, 0, canvas.width,canvas.height, 'black'); // draw background
-    colorRect(20, 200, 10, 100, 'white'); // draw paddle
-    colorCircle(ballX, 150, 10, 'white'); // draw ball
+    
+    colorRect(20, paddle1Y, 10, PADDLE_HEIGHT, 'white'); // draw paddle
+    
+    colorCircle(ballX, ballY, 10, 'white'); // draw ball
 }
 
 // Creates circles
